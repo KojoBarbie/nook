f6f5beaf9a1bef3122c8b1322148c530e6f3f29b
KojoBarbie <tmkhsy-0420@g.ecc.u-tokyo.ac.jp>
2025-03-06 23:54:23 +0900

    fix: フロントの構成も更新

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ContentResponse, WeatherResponse } from './types';
+import { ContentResponse, WeatherResponse } from '../../backend/src/frontend/src/types';
 
 const api = axios.create({
   baseURL: 'http://localhost:8000/api'
